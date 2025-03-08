539dddb5307df68b255e70692cb6bf987fb6ea27
joaoScheffel <joaololluc4s@gmail.com>
2025-03-08 19:50:37 -0300

    feat(accept-internal-user-invite.test.ts): added internal user validation

--- src/tests/application/accept-internal-user-invite.test.ts
+++ src/tests/application/accept-internal-user-invite.test.ts
@@ -1,8 +1,11 @@
 import * as crypto from "node:crypto";
 import AcceptInternalUserInvite from "../../application/accept-internal-user-invite";
 import {InternalUserInviteRepositoryFake} from "../../infra/internal-user-invite.repository";
-import {InternalUserPermission} from "../../domain/internal-user-invite";
 import InviteInternalUser from "../../application/invite-internal-user";
+import {InternalUserPermission} from "../../domain/internal-user";
+import InternalUserRepositoryFake from "../../infra/internal-user.repository";
+import {InternalUserInviteRepository} from "../../domain/internal-user-invite.repository";
+import {InternalUserRepository} from "../../domain/internal-user.repository";
 
 test("Deve aceitar um convite de usuário interno", async () => {
     const inviteInput = {
@@ -11,10 +14,11 @@ test("Deve aceitar um convite de usuário interno", async () => {
         invitedBy: crypto.randomUUID()
     }
 
-    const internalUserInviteRepositoryFake = new InternalUserInviteRepositoryFake()
+    const internalUserInviteRepository: InternalUserInviteRepository = new InternalUserInviteRepositoryFake()
+    const internalUserRepositoryFake: InternalUserRepository = new InternalUserRepositoryFake()
 
     const invitedInternalUser = await new InviteInternalUser(
-        internalUserInviteRepositoryFake
+        internalUserInviteRepository
     ).execute(inviteInput)
 
     const acceptInviteInput = {
@@ -22,12 +26,20 @@ test("Deve aceitar um convite de usuário interno", async () => {
     }
 
     await new AcceptInternalUserInvite(
-        internalUserInviteRepositoryFake
+        internalUserInviteRepository,
+        internalUserRepositoryFake
     ).execute(acceptInviteInput.inviteUuid)
 
-    const acceptedInvite = await internalUserInviteRepositoryFake.getInviteByUuid(
+    const acceptedInvite = await internalUserInviteRepository.getInviteByUuid(
         acceptInviteInput.inviteUuid
     )
 
+    const createdInternalUser = await internalUserRepositoryFake.getInternalUserByEmail(acceptedInvite.email)
+
     expect(acceptedInvite.isAccepted()).toBe(true)
+    expect(createdInternalUser.internalUserUuid).toBeDefined()
+    expect(createdInternalUser.email).toBe(acceptedInvite.email)
+    expect(createdInternalUser.username).toBeDefined()
+    expect(createdInternalUser.permission).toBe(acceptedInvite.permission)
+    expect(createdInternalUser.createdAt).toBeDefined()
 })

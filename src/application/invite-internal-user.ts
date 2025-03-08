bb2ab7c03a89f2edbf11a61493f1388492f75689
joaoScheffel <joaololluc4s@gmail.com>
2025-03-08 19:05:23 -0300

    refactor(invite-internal-user.ts): added InternalUserInvite build by factory method

--- src/application/invite-internal-user.ts
+++ src/application/invite-internal-user.ts
@@ -1,5 +1,4 @@
 import InternalUserInvite, {InternalUserPermission} from "../domain/internal-user-invite";
-import * as crypto from "node:crypto";
 import {InternalUserInviteRepository} from "../domain/internal-user-invite.repository";
 
 export default class InviteInternalUser {
@@ -9,8 +8,7 @@ export default class InviteInternalUser {
     }
 
     async execute(input: {email: string, permission: InternalUserPermission, invitedBy: string}) {
-        const newInternalUserInvite = new InternalUserInvite(
-            crypto.randomUUID(),
+        const newInternalUserInvite = InternalUserInvite.create(
             input.email,
             input.permission,
             input.invitedBy

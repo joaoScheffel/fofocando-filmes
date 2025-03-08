7dccdf6658a004dd95fb86275cf93dfb0d8d02d9
joaoScheffel <joaololluc4s@gmail.com>
2025-03-08 19:04:31 -0300

    feat(internal-user-invite.ts): implemented factory, accept and isAccepted methods

--- src/domain/internal-user-invite.ts
+++ src/domain/internal-user-invite.ts
@@ -1,10 +1,36 @@
+import * as crypto from "node:crypto";
+
 export default class InternalUserInvite {
     constructor(
         readonly inviteUuid: string,
         readonly email: string,
         readonly permission: InternalUserPermission,
-        readonly invitedBy: string
+        readonly invitedBy: string,
+        private acceptedAt: Date | null
     ) {}
+
+    static create(
+        email: string,
+        permission: InternalUserPermission,
+        invitedBy: string,
+    ): InternalUserInvite {
+        return new InternalUserInvite(
+            crypto.randomUUID(),
+            email,
+            permission,
+            invitedBy,
+            null
+        )
+    }
+
+    accept(): void {
+        if (this.isAccepted()) throw new Error("Invite already accepted")
+        this.acceptedAt = new Date()
+    }
+
+    isAccepted(): boolean {
+        return !!this.acceptedAt
+    }
 }
 
 export enum InternalUserPermission {

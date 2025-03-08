c39d1610a8afb288e2088e5ee11b8ca9723a8a3c
joaoScheffel <joaololluc4s@gmail.com>
2025-03-08 19:05:59 -0300

    feat(internal-user-invite.repository.ts): added getInviteByUuid and update methods

--- src/domain/internal-user-invite.repository.ts
+++ src/domain/internal-user-invite.repository.ts
@@ -2,4 +2,6 @@ import InternalUserInvite from "./internal-user-invite";
 
 export interface InternalUserInviteRepository {
     save(internalUserInvite: InternalUserInvite): Promise<void>
+    getInviteByUuid(inviteUuid: string): Promise<InternalUserInvite>
+    update(internalUserInvite: InternalUserInvite): Promise<void>
 }

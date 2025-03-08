116294d598695b7abf3cfb3ab3747665aa6103be
joaoScheffel <joaololluc4s@gmail.com>
2025-03-08 19:02:54 -0300

    chore(internal-user-invite.test.ts)

---
+++ src/tests/domain/internal-user-invite.test.ts
@@ -0,0 +1,59 @@
+import * as crypto from "node:crypto";
+import InternalUserInvite, {InternalUserPermission} from "../../domain/internal-user-invite";
+
+test("Deve instanciar um convite de usuário interno", () => {
+    const inviteInput = {
+        inviteUuid: crypto.randomUUID(),
+        email: 'jane.doe@gmail.com',
+        permission: InternalUserPermission.ADMIN,
+        invitedBy: crypto.randomUUID(),
+        acceptedAt: new Date()
+    }
+    const internalUserInvite = new InternalUserInvite(
+        inviteInput.inviteUuid,
+        inviteInput.email,
+        inviteInput.permission,
+        inviteInput.invitedBy,
+        inviteInput.acceptedAt
+    )
+    expect(internalUserInvite.inviteUuid).toBe(inviteInput.inviteUuid)
+    expect(internalUserInvite.email).toBe(inviteInput.email)
+    expect(internalUserInvite.permission).toBe(inviteInput.permission)
+    expect(internalUserInvite.invitedBy).toBe(inviteInput.invitedBy)
+    expect(internalUserInvite.isAccepted()).toBe(true)
+})
+
+test("Deve aceitar um convite de usuário interno", () => {
+    const inviteInput = {
+        inviteUuid: crypto.randomUUID(),
+        email: 'jane.doe@gmail.com',
+        permission: InternalUserPermission.ADMIN,
+        invitedBy: crypto.randomUUID()
+    }
+    const internalUserInvite = InternalUserInvite.create(
+        inviteInput.email,
+        inviteInput.permission,
+        inviteInput.invitedBy,
+    )
+    expect(internalUserInvite.isAccepted()).toBe(false)
+    internalUserInvite.accept()
+    expect(internalUserInvite.isAccepted()).toBe(true)
+})
+
+test("Não deve aceitar um convite de usuário interno que já está aceito", () => {
+    const inviteInput = {
+        inviteUuid: crypto.randomUUID(),
+        email: 'jane.doe@gmail.com',
+        permission: InternalUserPermission.ADMIN,
+        invitedBy: crypto.randomUUID()
+    }
+    const internalUserInvite = InternalUserInvite.create(
+        inviteInput.email,
+        inviteInput.permission,
+        inviteInput.invitedBy,
+    )
+    internalUserInvite.accept()
+
+    expect(internalUserInvite.isAccepted()).toBe(true)
+    expect(() => internalUserInvite.accept()).toThrow(new Error("Invite already accepted"))
+})

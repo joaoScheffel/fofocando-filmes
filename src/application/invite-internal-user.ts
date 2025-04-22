c4622c080df5c273570e54182152714afbc530ed
joaoScheffel <joaololluc4s@gmail.com>
2025-04-22 19:52:50 -0300

    feat(invite-internal-user.ts): added input and outputs interfaces

--- src/application/invite-internal-user.ts
+++ src/application/invite-internal-user.ts
@@ -1,22 +1,29 @@
-import InternalUserInvite from "../domain/internal-user-invite";
-import {InternalUserInviteRepository} from "../domain/internal-user-invite.repository";
-import {InternalUserPermission} from "../domain/internal-user";
+import InternalUserInvite from "../domain/entities/internal-user-invite";
+import {InternalUserInviteRepository} from "../domain/repositories/internal-user-invite.repository";
+import {InternalUserPermission} from "../domain/entities/internal-user";
 
 export default class InviteInternalUser {
     constructor(
         private internalUserInviteRepository: InternalUserInviteRepository
-    ) {
-    }
+    ) {}
 
-    async execute(input: {email: string, permission: InternalUserPermission, invitedBy: string}) {
+    async execute(input: InviteInternalUserInput): Promise<InviteInternalUserOutput> {
         const newInternalUserInvite = InternalUserInvite.create(
             input.email,
             input.permission,
             input.invitedBy
         )
-
         await this.internalUserInviteRepository.save(newInternalUserInvite)
-
-        return newInternalUserInvite
+        return {
+            inviteUuid: newInternalUserInvite.inviteUuid
+        }
     }
+}
+export interface InviteInternalUserInput {
+    email: string,
+    permission: InternalUserPermission,
+    invitedBy: string
+}
+export interface InviteInternalUserOutput {
+    inviteUuid: string
 }

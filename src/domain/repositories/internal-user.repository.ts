bf9baeb66c83a6aa230aab5911d9885fa9ff365f
joaoScheffel <joaololluc4s@gmail.com>
2025-04-22 19:52:01 -0300

    feat(internal-user.repository.ts): added getInternalUserByUuid method and moved

--- src/domain/repositories/internal-user.repository.ts
+++ src/domain/repositories/internal-user.repository.ts
@@ -1,6 +1,7 @@
-import InternalUser from "./internal-user";
+import InternalUser from "../entities/internal-user";
 
 export interface InternalUserRepository {
     save(internalUser: InternalUser): Promise<void>
     getInternalUserByEmail(email: string): Promise<InternalUser>
+    getInternalUserByUuid(internalUserUuid: string): Promise<InternalUser>
 }

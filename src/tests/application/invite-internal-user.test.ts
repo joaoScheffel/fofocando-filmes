003066ccc104857670397e17312f6d671db18f22
joaoScheffel <joaololluc4s@gmail.com>
2025-04-22 19:56:12 -0300

    feat(invite-internal-user.test.ts): added input and output usecase interfaces

--- src/tests/application/invite-internal-user.test.ts
+++ src/tests/application/invite-internal-user.test.ts
@@ -1,19 +1,20 @@
 import * as crypto from "node:crypto";
 import InviteInternalUser from "../../application/invite-internal-user";
-import {InternalUserInviteRepositoryFake} from "../../infra/internal-user-invite.repository";
-import {InternalUserPermission} from "../../domain/internal-user";
+import {InternalUserInviteRepositoryFake} from "../../infra/internal-user-invite-fake.repository";
+import {InternalUserPermission} from "../../domain/entities/internal-user";
+import {InternalUserInviteRepository} from "../../domain/repositories/internal-user-invite.repository";
 
 test('Deve convidar um usuário a ser interno', async () => {
+    const internalUserInviteRepository: InternalUserInviteRepository = new InternalUserInviteRepositoryFake()
     const inviteInput = {
         email: 'jane.doe@gmail.com',
         permission: InternalUserPermission.ADMIN,
         invitedBy: crypto.randomUUID()
     }
-
-    const invitedInternalUser = await new InviteInternalUser(
-        new InternalUserInviteRepositoryFake()
+    const {inviteUuid} = await new InviteInternalUser(
+        internalUserInviteRepository
     ).execute(inviteInput)
-
+    const invitedInternalUser = await internalUserInviteRepository.getInviteByUuid(inviteUuid)
     expect(invitedInternalUser.inviteUuid).toBeDefined()
     expect(invitedInternalUser.email).toBe(inviteInput.email)
     expect(invitedInternalUser.permission).toBe(inviteInput.permission)

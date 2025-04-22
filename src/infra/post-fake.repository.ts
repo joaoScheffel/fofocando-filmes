63de78c867c081ac896fe61d2654f633b371fd2e
joaoScheffel <joaololluc4s@gmail.com>
2025-04-22 19:51:04 -0300

    feat(post-fake.repository.ts): implemented fake repository

---
+++ src/infra/post-fake.repository.ts
@@ -0,0 +1,16 @@
+import {PostRepository} from "../domain/repositories/post.repository";
+import Post from "../domain/entities/post";
+
+export class PostFakeRepository implements PostRepository {
+    constructor(
+        public posts: Post[] = []
+    ) {}
+
+    async save(post: Post): Promise<void> {
+        this.posts.push(post)
+    }
+
+    async getPostByUuid(postUuid: string): Promise<Post> {
+        return this.posts.find((post) => post.postUuid === postUuid)
+    }
+}
